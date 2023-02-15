be544f2461ff4517075551d9e0e9649e40ae54a1
patilsubramanya <subramanya.patil@msds.christuniversity.in>
2023-02-15 14:26:46 +0530

    readme

---
+++ LAB_03/README.md
@@ -0,0 +1,17 @@
+Create a java program that performs the following operation:
+The program should collect an integer array from the user
+After the array is entered, you need to create a menu of items
+1)Find the Mean Value
+2)Find the Median Value
+3)Find the Mode Value
+From the user-given array.
+The program should have a minimum of 3 functions apart from the main function; each of the functions implements the Mean, Median, and Mode computation by accepting the array user has entered as a parameter and returning the value as a result. From the main method, you will display the result.
+ALGORITHM:-
+* Read the size of the array.
+* Enter the array elements.
+* Display a menu showing options for Mean, Median and Mode.
+* Ask for choice to the user.
+* Create a function which calculates the mean by adding all the elements of the array and dividing the sum by total number of elements.
+* Create another function which calculates median. Calculate the middle index of the array by dividing the last index by 2. Now display the element for that index in the array.
+* Create another fuction which calculates the mode. Iterate through all the elements in the array and compare the every element with every other element in the array.
+* Now create switch case through which the control goes to the particular function for which the use has opted.
--- LAB_03/lab_03.java
+++ LAB_03/lab_03.java
@@ -3,46 +3,58 @@ class lab_03{
     public static void main(String[] args){
         int condition=0;
         Scanner scan = new Scanner(System.in);
+        //asking the user to input the size of the array.
         System.out.println("Enter the size of the array: ");
         int n = Integer.parseInt(scan.nextLine());
+        //creting an array with the size of the user entered.
         int[] a = new int[n];
+        //asking the user to enter the elements of the array.
         System.out.println("Enter the values of the array: ");
         for(int i=0; i<a.length;i++){
             a[i] = Integer.parseInt(scan.nextLine());
         }
+        //displaying the entered array.
         System.out.println("The entered array is: ");
         for(int j=0;j<a.length;j++){
             System.out.println(a[j]);
         }
+        //do-while loop to create a menu for the user.
         do{
+            //displaying the menu.
             System.out.println("**********MENU**********");
             System.out.println("1.Mean\n2.Median\n3.Mode\n4.Exit");
             System.out.println("************************");
+            //Asking user the choice for the menu.
             System.out.println("Enter your choice: ");
             int ch =Integer.parseInt(scan.nextLine());
             switch(ch){
                 case 1:
+                    //Displaying the mean.
                     System.out.println("*********************");
                     System.out.println("The mean is: "+mean(a,n));
                     System.out.println("*********************");
                     break;
                 case 2:
+                    //Displaying the median.
                     System.out.println("*********************");
                     System.out.println("The median is: "+median(a));
                     System.out.println("*********************");
                     break;
                 case 3:
+                    //Displayig the mode.
                     System.out.println("*********************");
                     System.out.println("The mode is: "+mode(a,n));
                     System.out.println("*********************");
                     break;
                 case 4:
+                    //Exit
                     condition=5;
                     System.out.println("*********************");
                     System.out.println("EXITING...");
                     System.out.println("*********************");
                     break;
                 default:
+                    //Asking user to enter valid input if the user does not enter input between(1-4)
                     System.out.println("*********************");
                     System.out.println("Invalid Input!!");
                     System.out.println("*********************");
@@ -51,6 +63,7 @@ class lab_03{
         }while(condition<5);
         
     }
+    //function for mean
     static float mean(int a[], float b){
         int s=0;
         float m=0;
@@ -59,13 +72,14 @@ class lab_03{
         }
         m=s/b;
         return m;
-    }
+    }//function for median
     static int median(int a[]){
         int g=0,b=0;
         g=a.length;
         b=g/2;
         return a[b];
     }
+    //funtion for mode
     static int mode(int a[],int n) {
         int mv = 0, mc = 0, i, j;
   
